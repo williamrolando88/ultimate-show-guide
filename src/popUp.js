39f8a13ef3e85698ca2226fef376a780d6485fc6
Anjacodes <anja_schmidt7@posteo.de>
2021-12-22 16:02:20 -0600

    Get Comments From API

--- src/popUp.js
+++ src/popUp.js
@@ -1,11 +1,12 @@
 import getAPI from './apiRequest';
+import { getCommentsFromAPI, renderComments, appendComments } from './comments';
 import closePopUP from './interactions';
 import { createHTML, openPopUp } from './render';
 
-getAPI;
-createHTML;
-openPopUp;
-closePopUP;
+// getAPI;
+// createHTML;
+// openPopUp;
+// closePopUP;
 
 const renderPopUp = async (url) => {
   const seriesInfo = await getAPI(url);
@@ -14,10 +15,18 @@ const renderPopUp = async (url) => {
   return renderedHTML;
 };
 
+/**
+ * This function is called in openPopupWindow at index.js
+ * @param {string} url -> take url of movie at selected index
+ * @param {string} name -> take name of movie at selected index
+ */
+
 const popUpInteraction = async (url, name) => {
   const popUpWindow = await renderPopUp(url);
   closePopUP(popUpWindow);
-  console.log(name);
+  const commentInfo = await getCommentsFromAPI(name);
+  const commentSpan = renderComments(commentInfo);
+  appendComments(popUpWindow, commentSpan);
 };
 
 export default popUpInteraction;
