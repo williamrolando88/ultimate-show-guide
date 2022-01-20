bbc90dc861e4a4808acc29cabac48b91a16ccd54
williamrolando88 <william.rolando88@hotmail.com>
2022-01-20 17:33:46 -0500

    Updates

--- main.js
+++ main.js
@@ -275,24 +275,28 @@ const cardStructure = serie => ({
     class: [],
     children: [{
       tag: 'h2',
-      class: [],
+      class: ['text-3xl', 'font-rockwell', 'text-center', 'font-semibold', 'my-4'],
       textContent: serie.name
     }, {
       tag: 'div',
-      class: ['flex', 'justify-between', 'items-baseline'],
+      class: ['flex', 'items-stretch', 'gap-4'],
       children: [{
-        tag: 'i',
-        class: ['fas', 'fa-heart', 'text-red-600', 'cursor-pointer', 'text-xl']
+        tag: 'button',
+        class: ['grow', 'bg-stone-300', 'border', 'border-stone-300', 'rounded-md', 'font-rockwell', 'text-xl', 'hover:bg-stone-100', 'hover:shadow-md'],
+        textContent: 'Comment'
       }, {
-        tag: 'p',
-        class: [],
-        textContent: `${serie.likes} likes`
+        tag: 'div',
+        class: ['flex', 'flex-col', 'items-center', 'border', 'rounded-md', 'p-2'],
+        children: [{
+          tag: 'i',
+          class: ['fas', 'fa-heart', 'text-red-600', 'cursor-pointer', 'text-2xl', 'hover:scale-125']
+        }, {
+          tag: 'p',
+          class: ['font-bold'],
+          textContent: `${serie.likes} likes`
+        }]
       }]
     }]
-  }, {
-    tag: 'button',
-    class: ['bg-stone-300'],
-    textContent: 'Comment'
   }]
 });
 
@@ -496,32 +500,46 @@ const createHTML = ({
   medium,
   premiered
 }) => `
-    <i class="fas fa-times self-end py-6 text-4xl"></i>
-    <img src=${medium} alt="cover Image">
-    <h2 class='font-rockwell text-4xl font-bold py-6'>${name}</h2>
+    <i class="fas fa-times self-end py-4 text-4xl mr-8"></i>
+    <section class="flex w-[70%]">
+    <img src=${medium} class="mr-6 w-3/6" alt="cover Image">
+    <div class="flex flex-col">
+    <div class="flex flex-col mb-[50px]">
+    <h2 class='font-rockwell text-5xl font-bold py-6'>${name}</h2>
     <ul>
-      <li>Language: ${language}</li>
-      <li>Official Site: <a class='underline' href="${officialSite}">${officialSite}</a></li>
-      <li>Rating: ${average}</li>
-      <li>Premiered: ${premiered}</li>
+      <li class="font-black text-lg">Language: <span class="font-normal">${language}</span></li>
+      <li class="font-black text-lg">Official Site: <a class='font-normal underline hover:text-blue-600' href="${officialSite}">${officialSite}</a></li>
+      <li class="font-black text-lg">Rating: <span class="font-normal">${average}</span></li>
+      <li class="font-black text-lg">Premiered: <span class="font-normal">${premiered}</span></li>
     </ul>
-    <h3 class='font-rockwell text-2xl font-bold py-6'>Comments <span class="numComments">()</span></h3>
-    <div class='commentCont self-start flex flex-col'>
     </div>
-    <h3 class='font-rockwell text-2xl font-bold py-6'>Add new comment</h3>
-    <form class='flex flex-col self-start items-start'>
-      <input id='username' class='mb-4' placeholder='Your name here' required>
-      <textarea id='message' placeholder ='Your insights here' required></textarea>
-      <input class='bg-gray-300 commentBtn hover:bg-gray-400 text-gray-800 font-bold py-1 px-4 rounded mt-4' type='button' value='Comment'>
+
+    <div class="flex flex-col">
+    <h3 class='font-rockwell text-[2rem] font-bold py-6'>Add new comment</h3>
+    <form class='w-[100%] flex flex-col self-start items-start'>
+      <input id='username' class='mb-4 w-[100%]' placeholder='Your name here' required>
+      <textarea id='message' class='mb-4 w-[100%]' placeholder ='Your insights here' required></textarea>
+      <input class='bg-gray-300 commentBtn hover:bg-gray-400 text-gray-800 font-bold py-2 px-5 rounded mt-4' type='button' value='Comment'>
     </form>
+    </div>
+    </div>
+    </section>
+    <section class="py-[50px] flex justify-start w-[70%]">
+   
+    <div>
+    <h3 class='font-rockwell text-2xl font-bold py-6'>Comments <span class="numComments">()</span></h3>
+    <div class='commentCont text-lg self-start flex flex-col'>
+    </div>
+    </div>
+    </section>
     `;
 
 const openPopUp = obj => {
   const popUp = document.createElement('section');
   Object.assign(popUp, {
-    className: 'flex flex-col overflow-y-scroll items-center rounded-md px-[20%] py-6 my-[50px]'
+    className: 'flex flex-col overflow-y-scroll items-center rounded-md py-6 pb-6 my-[50px]'
   });
-  popUp.setAttribute('style', 'position : absolute; top: 65px; left: 20%; background-color: white');
+  popUp.setAttribute('style', 'position : absolute; top: 65px; width: 80vw; background-color: #e7e5e4');
   popUp.innerHTML = obj;
   main.appendChild(popUp);
   return popUp;
